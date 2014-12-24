ebfe03fe25315ee41314e2cc71b02e5280ce211c
otukutun <orehaorz@gmail.com>
2014-12-25 00:27:43 +0900

    fix

--- bot.rb
+++ bot.rb
@@ -20,7 +20,7 @@ end
 puts 'Configuration Success'
 
 
-TweetStream::Client.new.follow(90807562) do |status|
+TweetStream::Client.new.userstream do |status|
   # The status object is a special Hash with
   #   # method access to its keys.
   #client.update("test#{Time.now}")
