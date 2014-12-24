4a022ecd4e4b0b0ca2b856da31fb0174198ac146
otukutun <orehaorz@gmail.com>
2014-12-24 23:58:23 +0900

    fix

--- bot.rb
+++ bot.rb
@@ -2,12 +2,12 @@ require 'tweetstream'
 require 'twitter'
 require './weather.rb'
 
-streamclient = TweetStream.configure do |config|
+TweetStream.configure do |config|
   config.consumer_key       = ENV['CONSUMER_KEY']
   config.consumer_secret    = ENV['CONSUMER_SECRET']
   config.oauth_token        = ENV['ACCESS_KEY']
   config.oauth_token_secret = ENV['ACCESS_SECRET']
-  config.auth_method        = :oauth
+  #config.auth_method        = :oauth
 end
 
 client = Twitter::REST::Client.new do |config|
@@ -19,17 +19,34 @@ end
 
 puts 'Configuration Success'
 
-client.update("test#{Time.now}")
+#streamclient = TweetStream::Client.new
 
-puts 'Tweet success'
-
-streamclient.userstream do |status|
-  username = status[:user][:screen_name]
-  contents = status[:text]
-  id = status[:id]
-  str = username + ":" + contents
-  if username == 'otukutun'
-    client.update("test#{Time.now}")
-    puts str
-  end
+TweetStream::Client.new.sample do |status|
+  # The status object is a special Hash with
+  #   # method access to its keys.
+  client.update("test#{Time.now}")
+  puts "#{status.text}"
 end
+
+#client.update("test#{Time.now}")
+
+#streamclient.on_timeline_status  do |status|
+#    puts status.text
+#end
+
+#streamclient.userstream
+
+#puts 'Tweet success'
+
+
+#streamclient.userstream do |status|
+#  username = status[:user][:screen_name]
+#  contents = status[:text]
+#  id = status[:id]
+#  str = username + ":" + contents
+#  puts str
+#  #if username == 'otukutun'
+#  #  client.update("test#{Time.now}")
+#  #  puts str
+#  #end
+#end
